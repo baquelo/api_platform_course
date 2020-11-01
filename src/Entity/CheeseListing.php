deaea3abeb08e66910b34b233904ffd16c7df511
Marcelo Almeida <contato@marceloalmeida.dev>
2020-11-01 10:59:50 -0300

    configured formats of response

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -22,7 +22,11 @@ use Symfony\Component\Serializer\Annotation\SerializedName;
  *     itemOperations={"get", "put", "patch"},
  *     normalizationContext={"groups"={"cheese_listing:read"}, "swagger_definition_name"="Read"},
  *     denormalizationContext={"groups"={"cheese_listing:write"}, "swagger_definition_name"="Write"},
- *     shortName="cheese"
+ *     shortName="cheese",
+ *     attributes={
+ *         "pagination_items_per_page"=10,
+ *         "formats"={"jsonld", "json", "html", "jsonhal", "csv"={"text/csv"}}
+ *     }
  * )
  * @ORM\Entity(repositoryClass=CheeseListingRepository::class)
  * @ApiFilter(BooleanFilter::class, properties={"isPublished"})
